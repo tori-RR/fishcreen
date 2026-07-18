3d753a9e5ab2fcd5c3b190b81a6861da2f247f25
toriRR <tori-RR@users.noreply.github.com>
2026-07-19 06:17:47 +0800

    Set version to 1.0

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
